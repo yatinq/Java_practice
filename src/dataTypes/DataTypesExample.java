465e43051ea05a02f4e34ffbd08a3f493d196390
bijnis <bijnis@ip-192-168-1-2.ap-south-1.compute.internal>
2025-06-25 21:56:51 +0530

    data Types in Java

---
+++ src/dataTypes/DataTypesExample.java
@@ -0,0 +1,18 @@
+package dataTypes;
+
+
+public class DataTypesExample {
+    public static void main(String[] args) {
+        int age = 25;
+        float height = 5.9f;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Rahul";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}
