3826cb2953728cf2bbd190b7bd6aac4647f999fe
bijnis <bijnis@ip-192-168-1-87.ap-south-1.compute.internal>
2025-06-26 16:23:26 +0530

    add comment

--- src/dataTypes/SwapTwoNumbers.java
+++ src/dataTypes/SwapTwoNumbers.java
@@ -3,6 +3,8 @@ package dataTypes;
 public class SwapTwoNumbers {
 
 	public static void main(String[] args) {
+		
+		//Before
 		int a=5;
 		int b=3;
 		System.out.println("Before Swap: a = " + a + " b = " + b);
