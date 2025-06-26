57c0966caebb4f518e85d41e191b8dd18627435a
bijnis <bijnis@ip-192-168-1-87.ap-south-1.compute.internal>
2025-06-26 16:26:30 +0530

    add right comment

--- src/dataTypes/SwapTwoNumbers.java
+++ src/dataTypes/SwapTwoNumbers.java
@@ -4,7 +4,7 @@ public class SwapTwoNumbers {
 
 	public static void main(String[] args) {
 		
-		//Before
+		//Before swapping
 		int a=5;
 		int b=3;
 		System.out.println("Before Swap: a = " + a + " b = " + b);
