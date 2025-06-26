6fc5a5319c327a99b59f99b4bb2d514ddb1e0638
bijnis <bijnis@ip-192-168-1-2.ap-south-1.compute.internal>
2025-06-26 08:24:05 +0530

    type casting

---
+++ src/dataTypes/Type_Casting.java
@@ -0,0 +1,17 @@
+package dataTypes;
+
+public class Type_Casting {
+
+	public static void main(String[] args) {
+		int a =5;
+		float b=a;
+		
+		System.out.println("Int to float: "+b);
+		
+		float c=4.5f;
+		int d=(int) c;
+		System.out.println("Float to int: "+d);
+
+	}
+
+}
